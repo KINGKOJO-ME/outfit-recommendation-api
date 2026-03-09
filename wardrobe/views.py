21533be111f218c8528ba7cfae3e463a402d58b1
Kojo <ampah.kojo55@gmail.com>
2026-03-09 22:53:33 +0000

    Add season-based outfit recommendation

--- wardrobe/views.py
+++ wardrobe/views.py
@@ -11,15 +11,24 @@ class ClothingItemViewSet(viewsets.ModelViewSet):
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import ClothingItem
 
 @api_view(['GET'])
 def recommend_outfit(request):
-    
-    top = ClothingItem.objects.filter(category="top").first()
-    bottom = ClothingItem.objects.filter(category="bottom").first()
-    shoes = ClothingItem.objects.filter(category="shoes").first()
+
+    season = request.GET.get('season')
+
+    if season:
+        top = ClothingItem.objects.filter(category="top", season=season).first()
+        bottom = ClothingItem.objects.filter(category="bottom", season=season).first()
+        shoes = ClothingItem.objects.filter(category="shoes", season=season).first()
+    else:
+        top = ClothingItem.objects.filter(category="top").first()
+        bottom = ClothingItem.objects.filter(category="bottom").first()
+        shoes = ClothingItem.objects.filter(category="shoes").first()
 
     outfit = {
+        "season": season if season else "any",
         "top": top.name if top else "No top available",
         "bottom": bottom.name if bottom else "No bottom available",
         "shoes": shoes.name if shoes else "No shoes available"
