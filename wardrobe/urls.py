1248f906463142a2daab7da0a4d5b35220e245a5
Kojo <ampah.kojo55@gmail.com>
2026-03-09 22:15:56 +0000

    Add outfit recommendation API endpoint

--- wardrobe/urls.py
+++ wardrobe/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ClothingItemViewSet
+from .views import ClothingItemViewSet, recommend_outfit
 
 router = DefaultRouter()
 router.register(r'clothes', ClothingItemViewSet)
 
 urlpatterns = [
+    path('recommendations/', recommend_outfit),
     path('', include(router.urls)),
 ]
--- wardrobe/views.py
+++ wardrobe/views.py
@@ -5,3 +5,24 @@ from .serializers import ClothingItemSerializer
 class ClothingItemViewSet(viewsets.ModelViewSet):
     queryset = ClothingItem.objects.all()
     serializer_class = ClothingItemSerializer
+
+
+# Additional API view for outfit recommendation
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+def recommend_outfit(request):
+    
+    top = ClothingItem.objects.filter(category="top").first()
+    bottom = ClothingItem.objects.filter(category="bottom").first()
+    shoes = ClothingItem.objects.filter(category="shoes").first()
+
+    outfit = {
+        "top": top.name if top else "No top available",
+        "bottom": bottom.name if bottom else "No bottom available",
+        "shoes": shoes.name if shoes else "No shoes available"
+    }
+
+    return Response(outfit)
