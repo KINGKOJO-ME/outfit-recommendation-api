3b02757fc129313535f959e5dcff4275e4a245ab
Kojo <ampah.kojo55@gmail.com>
2026-03-09 21:38:29 +0000

    Create ClothingItem model and database migration

--- wardrobe/models.py
+++ wardrobe/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class ClothingItem(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=50)
+    season = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
