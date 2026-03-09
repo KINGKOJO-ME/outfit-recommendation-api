6448a03b9edad2baa27d7c403fc77f8f43d085f3
Kojo <ampah.kojo55@gmail.com>
2026-03-09 21:58:27 +0000

    Create clothing API using Django REST Framework

--- wardrobe/views.py
+++ wardrobe/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import ClothingItem
+from .serializers import ClothingItemSerializer
 
-# Create your views here.
+class ClothingItemViewSet(viewsets.ModelViewSet):
+    queryset = ClothingItem.objects.all()
+    serializer_class = ClothingItemSerializer
